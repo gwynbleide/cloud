515f52f96069fdf797426cd929c10eb140c860d8
Michal Barták <66036028+gwynbleide@users.noreply.github.com>
2024-07-16 16:51:55 +0200

    Update edge_uninstall.ps1

--- script/edge_uninstall.ps1
+++ script/edge_uninstall.ps1
@@ -3,3 +3,4 @@ $versionDir = Get-ChildItem -Path $basePath -Directory | Select-Object -First 1
 $fullPath = Join-Path -Path $basePath -ChildPath $versionDir.Name
 cd $fullPath
 .\setup.exe --uninstall --system-level --verbose-logging --force-uninstall
+Get-WmiObject -Class win32_Product | ft Name, Vendor, Version > C:\Windows\Logs\Installed_SW.txt
